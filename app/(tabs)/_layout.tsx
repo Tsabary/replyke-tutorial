b99e78e24844e28d2188d9c3d1a7afed1873d73f
Yanay Zabary <yanay.tsabary@gmail.com>
2025-01-18 11:08:18 +0200

    starting to add tabs bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import Entypo from "@expo/vector-icons/Entypo";
 
 export default function TabLayout() {
   return (
@@ -8,7 +9,51 @@ export default function TabLayout() {
         headerShown: false,
       }}
     >
-      <Tabs.Screen name="index" />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <Entypo name="home" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="lists"
+        options={{
+          title: "Lists",
+          tabBarIcon: ({ color }) => (
+            <Entypo name="home" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <Entypo name="home" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <Entypo name="home" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <Entypo name="home" size={28} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
---
+++ app/(tabs)/lists.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ListsScreen = () => {
+  return (
+    <View>
+      <Text>lists</Text>
+    </View>
+  )
+}
+
+export default ListsScreen
