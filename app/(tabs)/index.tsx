b04dbfb61ca8a8ecbf26e35b1122b0705849bf86
Yanay Zabary <yanay.tsabary@gmail.com>
2025-01-20 17:10:10 +0200

    signout and stuff

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,19 @@
 import { useRouter } from "expo-router";
 import { Button, View } from "react-native";
+import { useAuth, useUser } from "replyke-rn";
 
 export default function HomeScreen() {
+  const { signOut } = useAuth();
+  const { user } = useUser();
   const router = useRouter();
 
   return (
-    <View className="bg-red-500 flex-1 justify-center items-center">
-      <Button title="Sign in" onPress={() => router.navigate("/sign-in")} />
+    <View className="bg-red-500 flex-1 justify-center items-center gap-4">
+      {user ? (
+        <Button title="Sign out" onPress={signOut} />
+      ) : (
+        <Button title="Sign in" onPress={() => router.navigate("/sign-in")} />
+      )}
     </View>
   );
 }
